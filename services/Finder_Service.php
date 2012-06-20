199110f0d81e1d3c9b64c5c2d51b3ea80b7ae5d6
Rob Bayliss <rbayliss@rbayliss.net>
2012-06-20 15:13:45 -0400

    Updating services to latest definitions and use new librarygenerator code

--- services/Finder_Service.php
+++ services/Finder_Service.php
@@ -51,22 +51,22 @@ class Finder_Service extends SoapClient {
                                     'FinderCheckoutShoppingCartRequest' => 'FinderCheckoutShoppingCartRequest',
                                     'PaymentInfo' => 'PaymentInfo',
                                     'DebitAccountInfo' => 'DebitAccountInfo',
+                                    'CreditCardInfo' => 'CreditCardInfo',
                                     'CompInfo' => 'CompInfo',
                                     'StoredCardInfo' => 'StoredCardInfo',
-                                    'CreditCardInfo' => 'CreditCardInfo',
                                     'TrackDataInfo' => 'TrackDataInfo',
                                     'FinderCheckoutShoppingCartResponse' => 'FinderCheckoutShoppingCartResponse',
                                     'FinderCheckoutShoppingCartResult' => 'FinderCheckoutShoppingCartResult',
                                     'ShoppingCart' => 'ShoppingCart',
                                     'CartItem' => 'CartItem',
                                     'Item' => 'Item',
+                                    'Package' => 'Package',
+                                    'Service' => 'Service',
                                     'Product' => 'Product',
                                     'Color' => 'Color',
                                     'Size' => 'Size',
-                                    'Service' => 'Service',
-                                    'Package' => 'Package',
                                     'Tip' => 'Tip',
-                                    'Class' => 'Class',
+                                    'Class' => 'Mindbody_Class',
                                     'Visit' => 'Visit',
                                     'ClassDescription' => 'ClassDescription',
                                     'Level' => 'Level',
@@ -93,7 +93,7 @@ class Finder_Service extends SoapClient {
     }
     if(!ini_get('user_agent')) ini_set('user_agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.19) Gecko/20110707 Firefox/3.6.19');
     $directory = dirname(__FILE__).DIRECTORY_SEPARATOR;
-    foreach($options['classmap'] as $key => $value) if(file_exists("{$directory}../structures/{$value}.php")) include_once("{$directory}../structures/{$value}.php");
+    foreach($options['classmap'] as $key => $value) if(file_exists('{$directory}../structures/{$value}.php')) include_once('{$directory}../structures/{$value}.php');
     parent::__construct($wsdl, $options);
   }
 
